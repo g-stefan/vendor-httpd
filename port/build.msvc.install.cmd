1e2081a1a8d360e7c8881972f04608a136ecb317
Grigore Stefan <g_stefan@yahoo.com>
2020-10-02 16:54:30 +0300

    update

--- port/build.msvc.install.cmd
+++ port/build.msvc.install.cmd
@@ -6,8 +6,8 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 echo -^> install vendor-httpd
 
 set INSTALL_PATH=%XYO_PATH_REPOSITORY%
-set INSTALL_PATH_BIN=%INSTALL_PATH%
-set INSTALL_PATH_DEV=%INSTALL_PATH%
+set INSTALL_PATH_BIN=%INSTALL_PATH%/opt/httpd
+set INSTALL_PATH_DEV=%INSTALL_PATH%/opt/httpd
 
 rem // ---
 
